9b854ba5f81cc50f68b319b0914c6e7692248e83
eel3 <eel3@users.noreply.github.com>
2022-02-05 04:32:59 +0900

    Circular buffer: add maximum buffer size limit check

--- src/include/core/circular_buffer.hpp
+++ src/include/core/circular_buffer.hpp
@@ -10,6 +10,7 @@
 #include <atomic>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstring>
 #include <type_traits>
 
@@ -23,6 +24,7 @@ namespace cun {
 template <typename T, std::size_t N, typename IndexT =  std::atomic_size_t>
 class CircularBuffer final {
     static_assert(N > 0, "CircularBuffer: 0 size buffer is not allowed.");
+    static_assert(N < SIZE_MAX, "CircularBuffer: buffer size must be less than SIZE_MAX.");
 
 public:
     using size_type = std::size_t;
